b415e82b9a6420b375a2144b34b754708948509f
josegarban <36708774+josegarban@users.noreply.github.com>
2019-01-16 13:11:52 -0400

    Added functions to create .csv files from the dictionaries and lists created from the previous functions.

--- emailreader.py
+++ emailreader.py
@@ -1,4 +1,5 @@
 import pprint
+import csv
 import imaplib
 import getpass
 from datetime import datetime
@@ -12,7 +13,6 @@ La opción es "contraseñas para aplicaciones" bajo "autenticación en dos pasos
 """
 ####################################################################################################
 
-# User and pasword definitions
 def getcredentials():
     """
     Objective: obtaining e-mail and password in order to enter your e-mail
@@ -69,7 +69,7 @@ La opción es «contraseñas para aplicaciones» bajo «autenticación en dos pa
     typ, data = mail.search(None, "ALL")
     
     id_list  = data[0].split()        
-    earliest = int(id_list[-diff]) 
+    earliest = int(id_list[-int(diff)-1]) 
     latest   = int(id_list[-1]) 
     
     outputdict = {}
@@ -87,66 +87,66 @@ La opción es «contraseñas para aplicaciones» bajo «autenticación en dos pa
         print("Leyendo mensaje", latest - i + 1, "de", latest - earliest)
         
         for item in data:                        
-                if isinstance(item, tuple):
-                    message = email.message_from_string(item[1].decode())
-#                    print(message) 
-                    messagedict                 = {}
-                    messagedict["id"]           = i
-                    messagedict["from"]         = message["from"]
-                    messagedict["subject"]      = message["subject"]
-                    messagedict["delivered-to"] = message ["delivered-to"]
-                    messagedict["message-id"]   = message["message-id"]
+            if isinstance(item, tuple):
+                message = email.message_from_string(item[1].decode())
+#                print(message) 
+                messagedict                 = {}
+                messagedict["id"]           = i
+                messagedict["from"]         = message["from"]
+                messagedict["subject"]      = message["subject"]
+                messagedict["delivered-to"] = message ["delivered-to"]
+                messagedict["message-id"]   = message["message-id"]
 
-                    messagedict["date"]         = message["date"]
+                messagedict["date"]         = message["date"]
                     
-                    # Convert string to datetime in some commonly found datetime patterns
+                # Convert string to datetime in some commonly found datetime patterns                
+                try:
+                    datetime_patt = "%a, %d %b %Y %H:%M:%S %z"
+                    datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)            
+                except:
+                    pass
                 
-                    try:
-                        datetime_patt = "%a, %d %b %Y %H:%M:%S %z"
-                        datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)            
-                    except:
-                        pass
+                try:
+                    datetime_patt = "%a %b %d %H:%M:%S %Z %Y"
+                    datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)
+                except:
+                    pass
                 
-                    try:
-                        datetime_patt = "%a %b %d %H:%M:%S %Z %Y"
-                        datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)
-                    except:
-                        pass
-                
-                    try:
-                        datetime_patt = "%a, %d %b %Y %H:%M:%S %z (%Z)"
-                        datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)
-                    except:
-                        pass
+                try:
+                    datetime_patt = "%a, %d %b %Y %H:%M:%S %z (%Z)"
+                    datetime_conv = datetime.strptime(messagedict["date"], datetime_patt)
+                except:
+                    pass
                     
-                    try:
-                        datetime_conv = datetime_conv.replace(tzinfo=timezone.utc).astimezone(tz=None)
-#                        print(message["date"], "→", datetime_conv) # Test the datetime conversion
-                        messagedict["year"] = (datetime_conv.year)
-                        messagedict["month"] = (datetime_conv.month)
-                        messagedict["day"] = (datetime_conv.day)
-                        messagedict["time"] = (str(datetime_conv.hour) +":"+ str(datetime_conv.minute))                
-#                        print(messagedict["year"], messagedict["month"], messagedict["day"], messagedict["time"])                    
-                    except:
-                        # Print an error message and send the e-mail to the second dictionary
-                        print("No se pudo procesar la fecha en el mensaje", i)
-                        print("Fecha:", messagedict["date"])
-                        unprocesseddates.append((i, messagedict["date"]))
+                # Extract year, month, day from datetime stamp on emails
+                try:
+                    datetime_conv = datetime_conv.replace(tzinfo=timezone.utc).astimezone(tz=None)
+#                    print(message["date"], "→", datetime_conv) # Test the datetime conversion
+                    messagedict["year"] = (datetime_conv.year)
+                    messagedict["month"] = (datetime_conv.month)
+                    messagedict["day"] = (datetime_conv.day)
+                    messagedict["time"] = (str(datetime_conv.hour) +":"+ str(datetime_conv.minute))                
+#                    print(messagedict["year"], messagedict["month"], messagedict["day"], messagedict["time"])                    
+                except:
+                    # Print an error message and send the e-mail to the second dictionary
+                    print("No se pudo procesar la fecha en el mensaje", i)
+                    print("Fecha:", messagedict["date"])
+                    unprocesseddates.append((i, messagedict["date"]))
                     
-                    # Get the message body
-                    try:                                
-                        if message.is_multipart():
-                            messagedict["body"]     = ""
-                            for payload in message.get_payload():
-                                messagedict["body"] = messagedict["body"] + str(payload.get_payload())
-                        else:
-                            messagedict["body"]     = str(message.get_payload())
+                # Get the message body
+                try:                                
+                    if message.is_multipart():
+                        messagedict["body"]     = ""
+                        for payload in message.get_payload():
+                            messagedict["body"] = messagedict["body"] + str(payload.get_payload())
+                    else:
+                        messagedict["body"]     = str(message.get_payload())
                 
-                        outputdict[messagedict["id"]] = messagedict
-                    except:
-                        # Print an error message and send the e-mail to the third dictionary
-                        print("No se pudo procesar el cuerpo del mensaje", i)
-                        unprocessedbodies.append(i)                        
+                    outputdict[messagedict["id"]] = messagedict
+                except:
+                    # Print an error message and send the e-mail to the third dictionary
+                    print("No se pudo procesar el cuerpo del mensaje", i)
+                    unprocessedbodies.append(i)                        
 
     # Report to the user the result of their request
     print("Total de mensajes procesados:", len(outputdict), "(",
@@ -160,14 +160,100 @@ La opción es «contraseñas para aplicaciones» bajo «autenticación en dos pa
         pprint.pprint(unprocesseddates)
 
     if len(unprocessedbodies) == 0:
-        print("\nMensajes con cuerpos no procesadas:", len(unprocesseddates))
+        print("\nMensajes con cuerpos no procesados:", len(unprocessedbodies))
     else:
         print("\nMensajes con cuerpos no procesados:")
         print(unprocessedbodies)
 
-    return outputdict, unprocesseddates, unprocessedbodies
+    print(unprocesseddates, unprocessedbodies)
 
-test = readmail(getcredentials())
+    return (outputdict, unprocesseddates, unprocessedbodies)
+
+#test = readmail(getcredentials())
 
 ####################################################################################################
 
+def dictlist_to_csv (input_list, output_filename):
+    """
+    Objective: open a list containing dictionaries and write it to a csv file
+    Inputs:
+        (1) list containing dictionaries
+        (2) output csv filename
+    """
+    # Open (or create) csv file
+    with open (output_filename, "w") as csvfile:
+        writer = csv.writer (csvfile,
+                             delimiter = ";",
+                             quotechar = '"',
+                             quoting = csv.QUOTE_MINIMAL)
+        
+        for row in input_list:
+            writer.writerow(row)
+    
+    return None
+
+def nesteddict_to_csv (input_dict, output_filename):
+    """
+    Objective: open a dictionary containing dictionaries and write it to a csv file
+    Inputs:
+        (1) dictionary containing dictionaries
+        (2) output csv filename
+    """
+    # Open (or create) csv file
+    with open (output_filename, "w") as csvfile:
+        writer = csv.writer (csvfile,
+                             delimiter = ";",
+                             quotechar = '"',
+                             quoting = csv.QUOTE_MINIMAL)
+        
+        for key in input_dict.keys():
+            templist = []
+            for field in input_dict[key]:
+                templist.append((input_dict[key])[field])
+            writer.writerow(templist)
+    
+    return None
+
+####################################################################################################
+
+def save_mails_to_csvfiles ():
+    """
+    Objective: save the results of your inbox search to csv files
+    Input: user credentials
+    Returns: none
+    """
+    # Get user credentials to your e-mail account
+    credentials = getcredentials()
+    # Process the e-mails
+    mails       = readmail(credentials)
+    
+    # Pair arrays to filenames
+    processedmails    = mails[0]
+    procmails_fn      = "processedmails.csv"
+
+    unprocesseddates  = mails[1]
+    udates_fn         = "unprocesseddates.csv"
+
+    unprocessedbodies = mails[2]    
+    ubodies_fn        = "unprocessedbodies.csv"
+
+    tups = [(processedmails,  procmails_fn),
+            (unprocesseddates,   udates_fn),
+            (unprocessedbodies, ubodies_fn)
+            ]
+    
+    # Write files depending on whether we have a dictionary or a list
+    for tup in tups:
+        if   isinstance (tup[0], dict):
+            nesteddict_to_csv (tup[0], tup[1])
+            print("Se guardó el archivo", tup[1])
+        elif isinstance (tup[0], list):
+            dictlist_to_csv   (tup[0], tup[1])
+            print("Se guardó el archivo", tup[1])
+        else:
+            print("Error procesando el diccionario o lista.")
+    
+    
+test = save_mails_to_csvfiles ()
+
+#pprint.pprint (readmail(MYCREDENTIALS))
